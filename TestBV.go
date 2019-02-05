8ac5b48712991cc347d96920bcfe8f4989396bf2
Ricardo <ricardocastrom06@gmail.com>
2019-02-04 22:23:16 -0600

    connection to the database,get a list of genres,lenght and a total of songs

--- TestBV.go
+++ TestBV.go
@@ -18,12 +18,20 @@ type Song struct {
 	Length  int     "json:length"
 }
 
+type genreOrder struct {
+	
+	Count   int     "json:count"
+	Genre   string  "json:genre"
+	Length  int     "json:length"
+}
+
 func main() {
 	mux := goji.NewMux()
 	mux.HandleFunc(pat.Get("/artist/:artistName"), searchArtist)
 	mux.HandleFunc(pat.Get("/song/:songName"), searchSong)
 	mux.HandleFunc(pat.Get("/genre/:genreName"), searchGenre)
 	mux.HandleFunc(pat.Get("/length/:min/:max"), searchLength)
+	mux.HandleFunc(pat.Get("/order"), orderLength)
 	mux.Use(logging)
 	http.ListenAndServe("localhost:8000", mux)
 }
@@ -39,7 +47,6 @@ func Connection(w http.ResponseWriter, where string)(error){
     var name   string
     var length int
     list:= []Song{}
-	
     for rows.Next() {
         err = rows.Scan(&artist, &song,&name,&length)
         checkErr(err)
@@ -62,23 +69,19 @@ func Connection(w http.ResponseWriter, where string)(error){
 func searchArtist(w http.ResponseWriter, r *http.Request) {
 
 	artistName := pat.Param(r, "artistName")
-	Connection(w ,"songs.artist='"+artistName+"'" )// call the conection to the data base with the where condition for an artist search
-
-    
+	Connection(w ,"songs.artist='"+artistName+"'" )// call the conection to the data base with the where condition for an artist search  
 }
 
 func searchSong(w http.ResponseWriter, r *http.Request) {
 
 	song := pat.Param(r, "songName")
         Connection(w,"songs.song='"+song+"'")// call the conection to the data base with the where condition for a song search
-
 }
 
 func searchGenre(w http.ResponseWriter, r *http.Request) {
 
 	name := pat.Param(r, "genreName")
     	Connection(w,"genres.name='"+name+"'")// call the conection to the data base with the where condition for a genre search
-
 }
 
 func searchLength(w http.ResponseWriter, r *http.Request) {
@@ -86,7 +89,34 @@ func searchLength(w http.ResponseWriter, r *http.Request) {
 	min := pat.Param(r, "min")
 	max := pat.Param(r, "max")
 	Connection(w,"songs.length>="+min+" and songs.length<="+max)// call the conection to the data base with the where condition for a Length search
-    
+}
+
+func orderLength(w http.ResponseWriter, r *http.Request) {
+	
+    db, err := sql.Open("sqlite3", "./jrdd.db")
+    checkErr(err)
+    rows, err := db.Query("SELECT Count(songs.id),genres.name,Sum(songs.length) FROM genres inner join songs on genres.id=songs.genre group by genres.name ")
+    checkErr(err)
+    var count int
+    var genre string
+    var length int
+    list:= []genreOrder{}
+    for rows.Next() {
+        err = rows.Scan(&count, &genre,&length)
+        checkErr(err)
+        newGenre:=genreOrder{
+			
+			Count:      count,
+			Genre:     genre,
+			Length:    length,
+			}
+        list=append(list,newGenre)    
+    }
+
+    jsonOut, _ := json.Marshal(list)
+    fmt.Fprintf(w, string(jsonOut))
+    rows.Close() 
+    db.Close()
 }
 
 func logging(h http.Handler) http.Handler {
